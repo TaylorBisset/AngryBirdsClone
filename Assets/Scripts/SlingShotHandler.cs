0d24409121c958f2bf285a7c7e63e0807656f89d
Taylor Bisset <taylorbisset@gmail.com>
2024-03-07 22:48:45 -0800

    SlingShotHandler SetLines() initiated

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -5,15 +5,32 @@ using UnityEngine.InputSystem;
 
 public class SlingShot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LineRenderer leftLineRenderer;
+    private LineRenderer rightLineRenderer;
+
+    private Transform leftStartPosition;
+    private Transform rightStartPosition;
+
+    private void Update()
     {
-        
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            DrawSlingShot();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DrawSlingShot()
     {
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        SetLines(touchPosition);
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        leftLineRenderer.SetPosition(0, position);
+        leftLineRenderer.SetPosition(1, leftStartPosition.position);
         
+        rightLineRenderer.SetPosition(0, position);
+        rightLineRenderer.SetPosition(1, rightStartPosition.position);
     }
 }
