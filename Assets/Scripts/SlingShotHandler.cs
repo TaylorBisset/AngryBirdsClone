9e5a92a7d20adb01002ca1d68ccf6f3ead31c5b8
Taylor Bisset <taylorbisset@gmail.com>
2024-03-07 21:41:40 -0800

    Initial Commit

--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlingShotHandler : MonoBehaviour
+public class SlingShot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
